46af1ae86e11da775a2e42333e87ef3b1f3143d2
zunerz671 <zunerz671@gmail.com>
2025-10-06 18:51:48 +1000

    product model created and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_06_085039) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
